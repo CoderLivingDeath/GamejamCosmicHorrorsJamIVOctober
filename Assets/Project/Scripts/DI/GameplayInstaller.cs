776b1dc93e71aa718f5d0b1f345c5efd60b42e53
Eugene Nalivkin <CoderLivingDeath@gmail.com>
2025-10-13 23:23:50 +0700

    Add Location controller

--- Assets/Project/Scripts/DI/GameplayInstaller.cs
+++ Assets/Project/Scripts/DI/GameplayInstaller.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 using Zenject;
 
@@ -8,6 +9,12 @@ public class GameplayInstaller : ScriptableObjectInstaller<GameplayInstaller>
     public override void InstallBindings()
     {
         Container.Bind<Camera>().FromInstance(Camera.main).AsSingle();
+
+        // Найти все объекты MonoLocation на сцене, включая неактивные, в неотсортированном порядке (быстрее)
+        var locations = Object.FindObjectsByType<MonoLocation>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        // Создать словарь Id -> GameObject
+        var locationsDict = locations.ToDictionary(loc => loc.Id, loc => loc.gameObject);
 
         Container.BindFactory<MonoAudioSourcePool.Factory.CreateConfiguration, MonoAudioSourcePool, MonoAudioSourcePool.Factory>();
         Container.BindInterfacesAndSelfTo<AudioService>().FromFactory<AudioService, AudioServiceFactory>().AsCached().NonLazy();
--- Assets/Project/Scripts/LocationController.cs
+++ Assets/Project/Scripts/LocationController.cs
@@ -1,18 +1,21 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class LocationController
+public sealed class LocationController
 {
-    private Dictionary<string, GameObject> _locations;
+    public Dictionary<string, GameObject> Locations { get; private set; }
+    
+    public IEnumerable<KeyValuePair<string, GameObject>> ActiveLocations => Locations.Where(item => item.Value.activeSelf);
 
     public LocationController(Dictionary<string, GameObject> locations)
     {
-        _locations = locations;
+        Locations = locations;
     }
 
     public void HideLocation(string key)
     {
-        if (_locations.TryGetValue(key, out var location))
+        if (Locations.TryGetValue(key, out var location))
         {
             location.SetActive(false);
         }
@@ -24,7 +27,7 @@ public class LocationController
 
     public void ShowLocation(string key)
     {
-        if (_locations.TryGetValue(key, out var location))
+        if (Locations.TryGetValue(key, out var location))
         {
             location.SetActive(true);
         }
@@ -34,11 +37,52 @@ public class LocationController
         }
     }
 
-    public void ShowOnlyOneLocation(string key)
+    public void ActivateSingleLocation(string key)
     {
-        foreach (var kv in _locations)
+        if (!Locations.ContainsKey(key))
         {
-            kv.Value.SetActive(kv.Key == key);
+            Debug.LogWarning($"Location with key '{key}' not found");
+            return;
+        }
+
+        foreach (var kv in Locations)
+        {
+            bool shouldBeActive = kv.Key == key;
+            if (kv.Value.activeSelf != shouldBeActive)
+            {
+                kv.Value.SetActive(shouldBeActive);
+                Debug.Log($"SetActive({shouldBeActive}) for location '{kv.Key}'");
+            }
         }
     }
+    public void ActivateRangeLocation(IEnumerable<string> keys)
+    {
+        if (keys == null)
+        {
+            Debug.LogWarning("Keys collection is null");
+            return;
+        }
+
+        // Для удобства создадим HashSet для быстрого поиска
+        var keysSet = new HashSet<string>(keys);
+
+        // Проверим, есть ли хотя бы один ключ в _locations
+        bool keyFound = keysSet.Any(k => Locations.ContainsKey(k));
+        if (!keyFound)
+        {
+            Debug.LogWarning($"None of the specified keys found in locations");
+            return;
+        }
+
+        foreach (var kv in Locations)
+        {
+            bool shouldBeActive = keysSet.Contains(kv.Key);
+            if (kv.Value.activeSelf != shouldBeActive)
+            {
+                kv.Value.SetActive(shouldBeActive);
+                Debug.Log($"SetActive({shouldBeActive}) for location '{kv.Key}'");
+            }
+        }
+    }
+
 }
