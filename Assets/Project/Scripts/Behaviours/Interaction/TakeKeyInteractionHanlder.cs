1ff8a09355aa33a07478e27d7045aeba069958ba
Eugene Nalivkin <CoderLivingDeath@gmail.com>
2025-10-20 04:06:47 +0700

    add interactables prefab fix key interactableHandler

--- Assets/Project/Scripts/Behaviours/Interaction/TakeKeyInteractionHanlder.cs
+++ Assets/Project/Scripts/Behaviours/Interaction/TakeKeyInteractionHanlder.cs
@@ -5,10 +5,11 @@ namespace Project.Scripts.behaviours.Interaction.InteractableHandlers
     [InteractableComponent]
     public class TakeKeyInteractionHanlder : MonoInteractableHandlerBase
     {
+        public string KeyId = "Key_taked";
         public override void HandleInteract(InteractionContext context)
         {
             Debug.Log("Key taked:" + 1);
-            PlayerPrefs.SetInt("Key_taked", 1);
+            PlayerPrefs.SetInt(KeyId, 1);
         }
     }
 }
